938af35cb38bc846a745fd2af12e471705156bd2
codetech7 <codetechseven@gmail.com>
2022-05-19 10:50:58 +0100

    gotten private keys for youtube search

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
+const API_PRIVATE_KEY = 'AIzaSyCIiKrXKCgURNlqTQGtnvUZRcLuIVnVrLE';
 
 const App = () => {
   return <div>Hi there</div>;
 }
 
 
-ReactDOM.render(<App />, document.querySelector("container"));
+ReactDOM.render(<App />, document.querySelector(".container"));
