f547d3a9746953e140f49871844b51c05d1f2a4b
codetech7 <codetechseven@gmail.com>
2022-05-20 00:08:02 +0100

    Insert logic to track number of entries in input.
    The length of the entries that have been entered into the input tag is displayed.
    
    This is displayed below the input tag using the .length property.
    
    The .length property of the event target value is used to comput this.

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,44 +1,60 @@
 import { defaults } from 'lodash';
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 // const Searchbar = function () {
-   
+
 //         return <input  className= "search" />
-   
+
 // }
 
-class Searchbar extends Component{
-        constructor(props){
-                super(props);
+class Searchbar extends Component {
+    constructor(props) {
+        super(props);
 
-                this.state = {
-                        inputContent : '',
-                        inputClicks : 0,
-                        buttonClicks : 0
-                } 
-                
-                //the idea of states is that instead of creating several attribute variables, we can just create an object
-                  //or mapping to store all the attributes and call that the state.
+        this.state = {
+            inputContent: '',
+            inputClicks: 0,
+            buttonClicks: 0,
+             inputCount : 0
+            //  ()=>{
+            //     return inputContent.length;
+
+            // }
         }
 
-        render(){
-                return (
-                        <div>
-                                <input value = {this.state.inputContent} onChange={( event)=>{ //used to note when an input comes into the text field
-                                        // console.log(event.target.value);
-                                        this.setState({inputContent: event.target.value});
-                                }} onClick={function (event){
-                                        console.log(event.type);
-                                }}/>
+        //the idea of states is that instead of creating several attribute variables, we can just create an object
+        //or mapping to store all the attributes and call that the state.
+    }
+    
+    render() {
+        return (
+        <div>
+            <input value={this.state.inputContent}
+                onChange={
+                    (event) => { //used to note when an input comes into the text field
+                        // console.log(event.target.value);
+                        this.setState({ inputContent: event.target.value });
+                        this.setState({ inputCount: event.target.value.length });
+                    }
+                }
+                onClick={
+                    function (event) {
+                        console.log(event.type);
+                    }
+                }
+            />
 
-                                <button onClick={function (event){
-                                        console.log(event);
-                                }} />
+            <button onClick={
+                    function (event) {
+                        console.log(event);
+                    }
+                }
+            />
 
-                                <p value = ""/>
-                        </div>
-                )
-        }
+            <div > this input contains {this.state.inputCount} characters </div> 
+        </div>
+        )
+    }
 }
 
 export default Searchbar;
