23dd553b7e2f34eb689c81361b2f83a4e7cec86f
codetech7 <codetechseven@gmail.com>
2022-05-19 23:18:56 +0100

    Set input as a controlled component.
    Input gets its value from the inputContent state

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,44 @@
 import { defaults } from 'lodash';
-import React from 'react';
+import React, {Component} from 'react';
 
-const Searchbar = function () {
+// const Searchbar = function () {
    
-        return <input  className= "search" />
+//         return <input  className= "search" />
    
+// }
+
+class Searchbar extends Component{
+        constructor(props){
+                super(props);
+
+                this.state = {
+                        inputContent : '',
+                        inputClicks : 0,
+                        buttonClicks : 0
+                } 
+                
+                //the idea of states is that instead of creating several attribute variables, we can just create an object
+                  //or mapping to store all the attributes and call that the state.
+        }
+
+        render(){
+                return (
+                        <div>
+                                <input value = {this.state.inputContent} onChange={( event)=>{ //used to note when an input comes into the text field
+                                        // console.log(event.target.value);
+                                        this.setState({inputContent: event.target.value});
+                                }} onClick={function (event){
+                                        console.log(event.type);
+                                }}/>
+
+                                <button onClick={function (event){
+                                        console.log(event);
+                                }} />
+
+                                <p value = ""/>
+                        </div>
+                )
+        }
 }
 
 export default Searchbar;
