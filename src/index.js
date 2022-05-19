fe53e957e3f5f24dccb4d94e9cea1f2dad74a53c
codetech7 <codetechseven@gmail.com>
2022-05-19 02:17:21 +0100

    removed someboilerplate js files. build frm scrach

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+const React = require('react');
 
-import App from './components/app';
-import reducers from './reducers';
-
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+  return <div>Hi there</div>;
+}
+const app = new App();
+ReactDOM.render(app);
