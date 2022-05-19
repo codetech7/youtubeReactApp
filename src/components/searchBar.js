ddc4e8e9e794123bd649ad5924c2820a89c3bab9
codetech7 <codetechseven@gmail.com>
2022-05-19 21:53:25 +0100

    Add custom classname attribute to searchbar input
    Added a className attribute to try how normal html attribute work in JSX

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,8 +1,10 @@
 import { defaults } from 'lodash';
 import React from 'react';
 
-const Searchbar = () => {
-    return <input />
+const Searchbar = function () {
+   
+        return <input  className= "search" />
+   
 }
 
 export default Searchbar;
